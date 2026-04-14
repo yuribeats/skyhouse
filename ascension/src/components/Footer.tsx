e542a512a5721ba0676f4d99bde2613fc144c213
Yuri Rybak <w.yuri.rybak@gmail.com>
2026-04-14 14:09:55 -0400

    Remove 'brought to you' footer from all pages, clean up ascension footer

--- ascension/src/components/Footer.tsx
+++ ascension/src/components/Footer.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import SubscribeForm from "@/components/SubscribeForm";
 
 export default function Footer() {
@@ -8,27 +7,7 @@ export default function Footer() {
 
   return (
     <footer className="border-t border-neptune-blue/30">
-      <div className="flex justify-center bg-black py-4">
-        <Image
-          src="/assets/Stars__No_BG_Graphic_.png"
-          alt=""
-          width={400}
-          height={60}
-          className="h-[40px] w-auto opacity-60 mix-blend-screen"
-        />
-      </div>
-
       <div className="mx-auto max-w-[1200px] px-6 py-12 text-center md:px-12">
-        <div className="mb-8 flex justify-center">
-          <Image
-            src="/assets/Welcome_to_The_Ascension_Service__logo_.png"
-            alt="The Ascension Service"
-            width={120}
-            height={45}
-            className="h-auto w-[120px]"
-          />
-        </div>
-
         <div className="mx-auto mb-8 max-w-md">
           <SubscribeForm />
         </div>
