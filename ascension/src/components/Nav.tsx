77e523494554dcfe7753819cc4f792de7182d231
Yuri Rybak <w.yuri.rybak@gmail.com>
2026-04-15 16:59:07 -0400

    Hide globe on mobile

--- ascension/src/components/Nav.tsx
+++ ascension/src/components/Nav.tsx
@@ -43,7 +43,7 @@ export default function Nav() {
           </Link>
 
           {/* Neptune globe — center */}
-          <Link href="/" className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Link href="/" className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hidden md:block">
             <Image
               src="/assets/neptune-globe-cropped.png"
               alt="Neptune"
